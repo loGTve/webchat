7e18624bce0d0231fbc80d798a4c06f2a49040e9
WangJunYoung <logtve@naver.com>
2021-04-13 20:02:09 +0900

    a
    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import {Language} from '@styled-icons/fa-solid/Language';
+
+const Box = styled.div`
+    flex: 1;
+`
+
+const Welcome = styled.div`
+  position: absolute;
+  flex: 1;
+  margin-left: 38%;
+  margin-top: 25%;
+  font-family: Roboto;
+  
+  font-size: 40px;
+`
+
+const NameBox = styled.input`
+  
+  border-radius: 30px;
+  border-style: solid;
+  margin-left: 39%;
+  margin-top: 35%;
+  
+  font-size: 25px;
+  
+  padding-top: 10px;
+  
+  width: 300px;
+  height: 50px;
+`
+
+const LangChange = styled(Language)`
+  width: 100px;
+  height: 100px;
+  margin-left: 80%;
+  margin-top: 30px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box>
+          <LangChange/>
+          <Welcome>
+              이름을 입력해주세요.
+          </Welcome>
+          <NameBox/>
+      </Box>
   );
 }
 
